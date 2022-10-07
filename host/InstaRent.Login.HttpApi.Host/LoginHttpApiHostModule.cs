d30d1ec864c56f3cf696a152a33c57f9cea067a3
winkaykay <winkaykaykhine@gmail.com>
2022-10-07 22:32:26 +0800

    Remove sample

--- host/InstaRent.Login.HttpApi.Host/LoginHttpApiHostModule.cs
+++ host/InstaRent.Login.HttpApi.Host/LoginHttpApiHostModule.cs
@@ -13,16 +13,11 @@ using Volo.Abp.AspNetCore.Mvc.UI.MultiTenancy;
 using Volo.Abp.AspNetCore.Serilog;
 using Volo.Abp.AuditLogging.EntityFrameworkCore;
 using Volo.Abp.Autofac;
-//using Volo.Abp.Caching;
-//using Volo.Abp.Caching.StackExchangeRedis;
 using Volo.Abp.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore.SqlServer;
 using Volo.Abp.Modularity;
 using Volo.Abp.MultiTenancy;
-//using Volo.Abp.PermissionManagement.EntityFrameworkCore;
-//using Volo.Abp.SettingManagement.EntityFrameworkCore;
 using Volo.Abp.Swashbuckle;
-//using Volo.Abp.TenantManagement.EntityFrameworkCore;
 using Volo.Abp.VirtualFileSystem;
 
 namespace InstaRent.Login;
@@ -33,12 +28,8 @@ namespace InstaRent.Login;
     typeof(LoginHttpApiModule),
     typeof(AbpAspNetCoreMvcUiMultiTenancyModule),
     typeof(AbpAutofacModule),
-    //typeof(AbpCachingStackExchangeRedisModule),
     typeof(AbpEntityFrameworkCoreSqlServerModule),
     typeof(AbpAuditLoggingEntityFrameworkCoreModule),
-    //typeof(AbpPermissionManagementEntityFrameworkCoreModule),
-    //typeof(AbpSettingManagementEntityFrameworkCoreModule),
-    //typeof(AbpTenantManagementEntityFrameworkCoreModule),
     typeof(AbpAspNetCoreSerilogModule),
     typeof(AbpSwashbuckleModule)
     )]
@@ -72,11 +63,7 @@ public class LoginHttpApiHostModule : AbpModule
         }
 
         context.Services.AddAbpSwaggerGen(
-            //configuration["AuthServer:Authority"],
-            //new Dictionary<string, string>
-            //{
-            //    {"Login", "Login API"}
-            //},
+          
             options =>
             {
                 options.SwaggerDoc("v1", new OpenApiInfo { Title = "Login API", Version = "v1" });
@@ -84,49 +71,7 @@ public class LoginHttpApiHostModule : AbpModule
                 options.CustomSchemaIds(type => type.FullName);
                 options.HideAbpEndpoints();
             });
-
-        //Configure<AbpLocalizationOptions>(options =>
-        //{
-        //    options.Languages.Add(new LanguageInfo("ar", "ar", "العربية"));
-        //    options.Languages.Add(new LanguageInfo("cs", "cs", "Čeština"));
-        //    options.Languages.Add(new LanguageInfo("en", "en", "English"));
-        //    options.Languages.Add(new LanguageInfo("en-GB", "en-GB", "English (UK)"));
-        //    options.Languages.Add(new LanguageInfo("fi", "fi", "Finnish"));
-        //    options.Languages.Add(new LanguageInfo("fr", "fr", "Français"));
-        //    options.Languages.Add(new LanguageInfo("hi", "hi", "Hindi", "in"));
-        //    options.Languages.Add(new LanguageInfo("is", "is", "Icelandic", "is"));
-        //    options.Languages.Add(new LanguageInfo("it", "it", "Italiano", "it"));
-        //    options.Languages.Add(new LanguageInfo("hu", "hu", "Magyar"));
-        //    options.Languages.Add(new LanguageInfo("pt-BR", "pt-BR", "Português"));
-        //    options.Languages.Add(new LanguageInfo("ro-RO", "ro-RO", "Română"));
-        //    options.Languages.Add(new LanguageInfo("ru", "ru", "Русский"));
-        //    options.Languages.Add(new LanguageInfo("sk", "sk", "Slovak"));
-        //    options.Languages.Add(new LanguageInfo("tr", "tr", "Türkçe"));
-        //    options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "简体中文"));
-        //    options.Languages.Add(new LanguageInfo("zh-Hant", "zh-Hant", "繁體中文"));
-        //    options.Languages.Add(new LanguageInfo("de-DE", "de-DE", "Deutsch"));
-        //    options.Languages.Add(new LanguageInfo("es", "es", "Español"));
-        //});
-
-        //context.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        //    .AddJwtBearer(options =>
-        //    {
-        //        options.Authority = configuration["AuthServer:Authority"];
-        //        options.RequireHttpsMetadata = Convert.ToBoolean(configuration["AuthServer:RequireHttpsMetadata"]);
-        //        options.Audience = "Login";
-        //    });
-
-        //Configure<AbpDistributedCacheOptions>(options =>
-        //{
-        //    options.KeyPrefix = "Login:";
-        //});
-
-        //var dataProtectionBuilder = context.Services.AddDataProtection().SetApplicationName("Login");
-        //if (!hostingEnvironment.IsDevelopment())
-        //{
-        //    var redis = ConnectionMultiplexer.Connect(configuration["Redis:Configuration"]);
-        //    dataProtectionBuilder.PersistKeysToStackExchangeRedis(redis, "Login-Protection-Keys");
-        //}
+         
 
         context.Services.AddCors(options =>
         {
@@ -162,27 +107,20 @@ public class LoginHttpApiHostModule : AbpModule
             app.UseHsts();
         }
 
-        //app.UseHttpsRedirection();
         app.UseCorrelationId();
         app.UseStaticFiles();
         app.UseRouting();
         app.UseCors();
-        //app.UseAuthentication();
         if (MultiTenancyConsts.IsEnabled)
         {
             app.UseMultiTenancy();
         }
         app.UseAbpRequestLocalization();
-        //app.UseAuthorization();
         app.UseSwagger();
         app.UseAbpSwaggerUI(options =>
         {
             options.SwaggerEndpoint("/swagger/v1/swagger.json", "Login API");
-
-            //var configuration = context.GetConfiguration();
-            //options.OAuthClientId(configuration["AuthServer:SwaggerClientId"]);
-            //options.OAuthClientSecret(configuration["AuthServer:SwaggerClientSecret"]);
-            //options.OAuthScopes("Login");
+            
         });
         app.UseAuditing();
         app.UseAbpSerilogEnrichers();
